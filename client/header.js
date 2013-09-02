158d471b6435ce57544aae07a35c6b7b0bc4b89c
Timur Bazhirov <timur.bazhirov@gmail.com>
2013-09-02 02:30:41 -0700

    spiderable package added, app cleaned up and brushed up

---
+++ client/header.js
@@ -0,0 +1,17 @@
+Template.header.helpers({
+	scholar: function () {
+		return GOOGLE_SCHOLAR_URL;
+	},
+	facebook: function () {
+		return GOOGLE_SCHOLAR_URL;
+	},
+	linkedin: function () {
+		return GOOGLE_SCHOLAR_URL;
+	},
+	soundcloud: function () {
+		return GOOGLE_SCHOLAR_URL;
+	},
+	github: function () {
+		return GOOGLE_SCHOLAR_URL;
+	}
+});
---
+++ client/social_links.js
@@ -0,0 +1,6 @@
+GOOGLE_SCHOLAR_URL = "http://scholar.google.com/citations?user=7SxfHbMAAAAJ";
+FACEBOOK_URL 			 = "https://www.facebook.com/timur.bazhirov";
+LINKEDIN_URL			 = "http://www.linkedin.com/profile/view?id=86118468";
+SOUNDCLOUD_URL		 = "https://soundcloud.com/thegraphtheory";
+GITHUB_URL				 = "https://github.com/timurbazhirov";
+TWITTER_URL				 = "https://twitter.com/TimurBazhirov";
